66c92fced040ce5e96928392a7394e68a43cf8fa
Kok Ben Toh <kokbent@ufl.edu>
2020-03-16 20:10:27 -0400

    Separate code for visualization

--- code/allocate_hh.R
+++ code/allocate_hh.R
@@ -8,7 +8,6 @@ library(doSNOW)
 source("code/data_path.R")
 
 cenacs <- shapefile(fl_cenacs)
-fl_cnt_shp <- shapefile(fl_cnt)
 fl_hh_r <- raster(fl_hh)
 cenacs_wgs <- cenacs %>% spTransform(crs(fl_hh_r))
 
@@ -143,15 +142,3 @@ write_csv(hh_xy_puma %>% select(HID, x, y),
           "output/hh_coords.csv")
 write_csv(person_xy %>% select(PID, HID, x, y, SEX, AGE, SCHOOL, EMPSTATD, PWSTATE2, PWPUMA00),
           "output/person_details.csv")
-
-#### Visualization
-png("fig/hh_proposed.png", 2400, 2400, pointsize = 10, res = 300)
-hh_sp <- SpatialPoints(hh_xy[,c("x", "y")], proj4string = crs(fl_hh_r))
-plot(hh_sp, pch = ".", col = rgb(1, 0, 0, alpha = 0.2))
-plot(fl_cnt_shp %>% spTransform(crs(fl_hh_r)), add = T)
-dev.off()
-
-tmp <- rasterize(hh_sp, fl_hh_r, fun = 'count')
-png("fig/hh_density.png", 2400, 2400, pointsize = 10, res = 300)
-plot(tmp)
-dev.off()
---
+++ code/visualize_hh.R
@@ -0,0 +1,21 @@
+rm(list = ls())
+
+library(raster)
+library(tidyverse)
+
+source("code/data_path.R")
+
+fl_cnt_shp <- shapefile(fl_cnt)
+hh_xy <- read_csv("output/hh_coords.csv")
+
+#### Visualization
+png("fig/hh_proposed.png", 2400, 2400, pointsize = 10, res = 300)
+hh_sp <- SpatialPoints(hh_xy[,c("x", "y")], proj4string = crs(fl_hh_r))
+plot(hh_sp, pch = ".", col = rgb(1, 0, 0, alpha = 0.2))
+plot(fl_cnt_shp %>% spTransform(crs(fl_hh_r)), add = T)
+dev.off()
+
+tmp <- rasterize(hh_sp, fl_hh_r, fun = 'count')
+png("fig/hh_density.png", 2400, 2400, pointsize = 10, res = 300)
+plot(tmp)
+dev.off()
