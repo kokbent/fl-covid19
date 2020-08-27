d022f0d13ae1952dbca623e18be21d5b335f56a0
Kok Ben Toh <kokbent@ufl.edu>
2020-08-27 03:42:36 -0400

    Try to optimize build_neighbour_network

--- state-sim/code/build_neighbour_network.R
+++ state-sim/code/build_neighbour_network.R
@@ -8,7 +8,7 @@ library(doSNOW)
 source("state-sim/code/data_path.R")
 
 coeff <- 0.45
-ncore <- 5
+ncore <- 4
 
 #### Build household network
 pers <- fread("state-sim/output/pers_w_wid.csv")
@@ -51,7 +51,7 @@ hh_wneighbour <- hh_nonnh %>%
 repl <- rep(1:nrow(hh_wneighbour), hh_wneighbour$neighbour)
 pts_mat <- hh_wneighbour[repl, c("HID", "x", "y")] %>% as.matrix
 rownames(pts_mat) <- NULL
-loc_mat <- hh_wneighbour[, c("x", "y")] %>% as.matrix
+loc_mat <- hh_wneighbour[, c("HID", "x", "y")] %>% as.matrix
 weights <- hh_wneighbour$neighbour
 
 
@@ -59,6 +59,9 @@ slice_size <- floor(nrow(pts_mat) / ncore)
 df <- data.frame(start = 0:(ncore - 1) * slice_size + 1,
                  end = 1:ncore * slice_size)
 df$end[ncore] <- nrow(pts_mat)
+
+rm(pers, hh, nh, hh_nonnh, hh_nh, hh_spdf)
+print(df)
 
 ## Run away with it!
 cl <- makeCluster(ncore)
@@ -69,13 +72,13 @@ system.time({
                        .packages = "abmgravity") %dopar% {
                          pts_mat_sub <- pts_mat[df$start[i]:df$end[i],]
                          assign_mat <- assign_by_gravity(pts = pts_mat_sub[,2:3], 
-                                                         locs = loc_mat, 
+                                                         locs = loc_mat[,2:3], 
                                                          weights = weights,
                                                          num_loc = 1000, 
                                                          seed = 4326 + i * 28, 
                                                          steps = 1)
                          hid1 <- pts_mat_sub[assign_mat[,1], 1]
-                         hid2 <- hh_wneighbour$HID[assign_mat[,2]]
+                         hid2 <- loc_mat[,1][assign_mat[,2]]
                          
                          hid_mat <- cbind(hid1, hid2)
                          hid_mat
