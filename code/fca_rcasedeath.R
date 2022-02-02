bc4953269a74a69171546c50729e9dc2911bd51c
Ben Toh <kokbent@ufl.edu>
2022-02-02 11:20:53 -0600

    Add FCA data plus update ts through 2022-02-02

--- code/fca_rcasedeath.R
+++ code/fca_rcasedeath.R
@@ -1,8 +1,7 @@
 #### Death by report day
-library(tidyverse)
+library(dplyr)
 library(data.table)
 library(lubridate)
-library(googlesheets4)
 
 daily <- fread("data/FCA_daily.csv")
 cnt <- fread("data/FCA_county.csv")
@@ -13,39 +12,19 @@ daily_w_cnt <- cnt %>%
 daily_w_cnt$Date <- ymd_hms(daily_w_cnt$Date) %>% as.Date()
 
 #### Choice of counties
-counties <- list()
-counties$Escambia <- "Escambia"
-counties$Dade <- "Dade"
-counties$Duval <- "Duval"
-counties$Leonplus <- c("Jackson", "Calhoun", "Gulf", "Gadsden", "Liberty", 
-                       "Franklin", "Leon", "Wakulla", "Jefferson")
-counties$Broward <- c("Broward")
-counties$Sumterplus <- c("Sumter", "Lake")
-counties$Florida <- unique(daily_w_cnt$County)
-
-for (i in 1:length(counties)) {
-  nombre <- names(counties)[i]
-  cnts <- counties[[i]]
-  
   df <- daily_w_cnt %>%
-    filter(County %in% cnts) %>%
+    filter(County != "Unknown") %>%
     group_by(Date) %>%
     summarise(rcase = sum(cases), rdeath = sum(deaths_rep)) %>%
     arrange(Date)
   
-  # if(i == 7) {
-  #   df <- df %>% left_join(dat[,c("Date", "RNewHosp")]) %>%
-  #     rename(rhosp = RNewHosp)
-  # }
-  
   df <- df %>%
     filter(Date <= ymd("2021-05-26"))
-  outfile <- paste0("ts/rcasedeath-", tolower(nombre), ".csv")
+  outfile <- "ts/rcasedeath-florida.csv"
   fwrite(df, outfile)
-}
 
 #### Merging with CDC data
-cdc_data <- fread("data/data_table_for_daily_case_trends__florida3.csv")
+cdc_data <- fread("data/data_table_for_daily_case_trends__florida.csv")
 cdc_data$Date <- mdy(cdc_data$Date)
 cdc_data <- cdc_data %>%
   filter(Date > ymd("2021-05-26")) %>%
@@ -55,7 +34,7 @@ df <- df %>%
   arrange(Date)
 plot(df$Date, df$rcase, type="l")
 
-cdc_death <- fread("data/data_table_for_daily_death_trends__florida3.csv")
+cdc_death <- fread("data/data_table_for_daily_death_trends__florida.csv")
 cdc_death$Date <- mdy(cdc_death$Date)
 cdc_death <- cdc_death %>%
   select(Date, death_incd = `New Deaths`)
