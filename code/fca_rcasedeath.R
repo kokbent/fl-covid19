53860f2e609334f155689ed42b80b23d24032b28
kokbent <kokbent@ufl.edu>
2022-04-05 23:59:39 -0500

    Add excess deaths to TS

--- code/fca_rcasedeath.R
+++ code/fca_rcasedeath.R
@@ -12,16 +12,16 @@ daily_w_cnt <- cnt %>%
 daily_w_cnt$Date <- ymd_hms(daily_w_cnt$Date) %>% as.Date()
 
 #### Choice of counties
-  df <- daily_w_cnt %>%
-    filter(County != "Unknown") %>%
-    group_by(Date) %>%
-    summarise(rcase = sum(cases), rdeath = sum(deaths_rep)) %>%
-    arrange(Date)
-  
-  df <- df %>%
-    filter(Date <= ymd("2021-05-26"))
-  outfile <- "ts/rcasedeath-florida.csv"
-  fwrite(df, outfile)
+df <- daily_w_cnt %>%
+  filter(County != "Unknown") %>%
+  group_by(Date) %>%
+  summarise(rcase = sum(cases), rdeath = sum(deaths_rep)) %>%
+  arrange(Date)
+
+df <- df %>%
+  filter(Date <= ymd("2021-05-26"))
+outfile <- "ts/rcasedeath-florida.csv"
+fwrite(df, outfile)
 
 #### Merging with CDC data
 cdc_data <- fread("data/data_table_for_daily_case_trends__florida.csv")
@@ -42,4 +42,44 @@ cdc_death <- cdc_death %>%
 df <- df %>%
   left_join(cdc_death)
 
-fwrite(df, outfile)
+#### Excess death
+excess <- fread("https://data.cdc.gov/api/views/xkkf-xrst/rows.csv?accessType=DOWNLOAD&bom=true&format=true%20target=")
+excess1 <- excess %>%
+  filter(State == "Florida", Type == "Predicted (weighted)", Outcome == "All causes") %>%
+  mutate(week_end_date = ymd(`Week Ending Date`),
+         Week = epiweek(week_end_date),
+         Year = ifelse(Week %in% 52:53 & month(week_end_date) == 1,
+                       year(week_end_date) - 1,
+                       year(week_end_date)))
+
+excess1 <- excess1 %>%
+  group_by(week_end_date, Week, Year) %>%
+  summarise(n = sum(`Observed Number`))
+
+excess2 <- excess1 %>%
+  group_by(Week) %>%
+  mutate(exp_n = mean(n[Year < 2020])) %>%
+  ungroup() %>%
+  mutate(exp_n = ifelse(!is.na(exp_n), exp_n, exp_n[Week == 52][1])) %>%
+  filter(week_end_date >= ymd("2020-01-11"), week_end_date <= ymd("2022-02-05")) %>%
+  mutate(diff = n - exp_n,
+         base_diff = mean(diff[week_end_date <= ymd("2020-03-28")]),
+         adj_diff = diff - base_diff)
+
+df1 <- df %>%
+  mutate(Week = epiweek(Date),
+         Year = ifelse(Week %in% 52:53 & month(Date) == 1,
+                       year(Date) - 1,
+                       year(Date)))
+
+df1 <- df1 %>%
+  group_by(Year, Week) %>%
+  mutate(week_end_date = max(Date))
+df2 <- left_join(df1, 
+                 excess2 %>% select(week_end_date, excess=adj_diff)) %>%
+  mutate(excess = excess / 7)
+
+plot(df2$Date, df2$excess, type = "l")
+
+fwrite(df2 %>% ungroup %>%
+         select(-Week, -Year, -week_end_date), outfile)
